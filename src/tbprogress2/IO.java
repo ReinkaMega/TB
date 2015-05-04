6c51fe2bb865e8f27dde9640b51ecf2032dc5468
Sanda <Sanda@Sanda-PC>
2015-05-04 21:16:18 +0700

    test

---
+++ src/tbprogress2/IO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tbprogress2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author toshiba
+ */
+public class IO extends Sistem {
+
+}
+
---
+++ src/tbprogress2/TBProgress2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tbprogress2;
+/**
+ *
+ * @author toshiba
+ */
+public class TBProgress2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Sistem kk = new Sistem();
+        IO ll = new IO();
+        kk.inputIdentitasKelas();
+        kk.InputKondisiRuang();
+        kk.tampil();
+    }
+}
