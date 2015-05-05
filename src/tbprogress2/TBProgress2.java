53d5e69afee28ef89b5f4068926a402b4b21591b
Deny Q <>
2015-05-05 08:31:36 +0700

    test

--- src/tbprogress2/TBProgress2.java
+++ src/tbprogress2/TBProgress2.java
@@ -13,13 +13,11 @@ public class TBProgress2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
         Sistem kk = new Sistem();
         IO ll = new IO();
         kk.inputIdentitasKelas();
         kk.InputKondisiRuang();
         kk.CheckKondisiRuang();
         kk.tampil();
-        
     }
 }
