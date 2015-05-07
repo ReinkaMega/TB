d334ae8810e32b6fc976515f273d2e2c1d8b0c7e
Deny SQP <Deny SQP@172.16.13.172>
2015-05-07 20:46:12 +0700

    tes

--- src/tbprogress2/TBProgress2.java
+++ src/tbprogress2/TBProgress2.java
@@ -8,14 +8,12 @@ package tbprogress2;
  * @author toshiba
  */
 public class TBProgress2 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Sistem analisis = new Sistem();
-        int hit = 0;
-        hit += (int) analisis.CheckKondisiRuang();
+        analisis.CheckKondisiRuang();
         //Arinkade
     }
 }
