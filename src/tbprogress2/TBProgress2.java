02aebcda669f5c36913fd1b3ee0f85c3d0c16ce1
DenyQP <denyqp1@gmail.com>
2015-05-14 19:30:58 +0700

    Deny Rere Eko

--- src/tbprogress2/TBProgress2.java
+++ src/tbprogress2/TBProgress2.java
@@ -9,7 +9,7 @@ package tbprogress2;
  */
 public class TBProgress2 {
    
-    /**i
+    /**inyuk
      * @param args the command line arguments 
      */
     public static void main(String[] args) {
